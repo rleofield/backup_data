499c70fa8259b927fe25115897d3f434e943bc95
rleofield <richard@rleofield.de>
2023-01-06 19:55:28 +0100

    modified sh scripts 06.01.2023

--- backup_data_geaendert/bk_archive.sh
+++ backup_data_geaendert/bk_archive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # file: bk_archive.sh
-# bk_version 22.08.1
+# bk_version 23.01.1
 
 
-# Copyright (C) 2021 Richard Albrecht
+# Copyright (C) 2017-2023 Richard Albrecht
 # www.rleofield.de
 
 # This program is free software: you can redistribute it and/or modify
